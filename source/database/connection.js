6597c78cc61ec9e8c793e7773a06729fa3857aed
animeshdixit <animeshdixit09@gmail.com>
2022-05-29 01:23:17 +0530

    connection done orders successfull

--- source/database/connection.js
+++ source/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Cafe").then(() =>{
+mongoose.connect("mongodb://localhost:27017/FoodGarage").then(() =>{
     console.log("connection successfull");
 }).catch((e)=>{
     console.log("connection unscuccessful")
--- source/models/orders.js
+++ source/models/orders.js
@@ -1,29 +1,65 @@
 const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true
-    },
-    lastname:{
-        type:String,
-        required:true
-    },
-    order:{
-        type:String,
-        required:true,
-        
-    },
-    phone:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    address:{
-        type:String,
-        required:true
-    },
-   
-})
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+//   soups: {
+//     type: String,
+//   },
+//   garlicBread: {
+//     type: String,
+//   },
+//   noodles: {
+//     type: String,
+//   },
+//   fries: {
+//     type: String,
+//   },
+//   dals: {
+//     type: String,
+//   },
+//   rice: {
+//     type: String,
+//   },
+//   roti: {
+//     type: String,
+//   },
+//   sabzi: {
+//     type: String,
+//   },
+//   pizza: {
+//     type: String,
+//   },
+//   burger: {
+//     type: String,
+//   },
+//   shake: {
+//     type: String,
+//   },
+  bread: {
+    type: String,
+  },
+  garlic: {
+    type: String,
+  },
+  food: {
+    type: String,
+  },
+//   phone: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+  address: {
+    type: String,
+    required: true,
+  },
+});
 
-const Orders = new mongoose.model("Orders",orderSchema);
-module.exports=Orders;
+const Orders = new mongoose.model("Orders", orderSchema);
+module.exports = Orders;
